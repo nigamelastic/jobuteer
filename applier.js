303f606521b2e7f881cdf6554599b9e5672291fd
nigamelastic <nigamelastic@gmail.com>
2018-11-28 18:04:25 +0100

    added the feature to login prior

--- applier.js
+++ applier.js
@@ -5,17 +5,30 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage()
   
   //await page.screenshot({ path: 'keyboard.png' })
+
+
+  await page.goto('https://linkedin.com');
+  await page.focus('input.login-email');
+  await page.type('login'); // your login here
+  await page.focus('input.login-password');
+  await page.type('password'); // your password here
+  page.click('.submit-button');
+  await page.waitForNavigation();
+  console.log('done!');
+ 
   await page.goto('https://www.linkedin.com/jobs/')
   
     // Type location
+    await page.evaluate(function() {
+      document.querySelector('input#location-search-box').value = ''
+      })
     await page.type('#location-search-box', 'Vienna');
     await page.waitFor(3000);
     await page.keyboard.press('ArrowDown');
-    await page.waitFor(3000);
     await page.keyboard.press('ArrowDown');
-    await page.waitFor(3000);
+    await page.waitFor(1000);
     await page.keyboard.press('Enter');
-    await page.waitFor(3000);
+    await page.keyboard.press('Enter');
   
 
   //await browser.close()
