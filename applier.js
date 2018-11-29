63ad14a02f6920232fb825e77e1851ffde24e8f6
nigamelastic <nigamelastic@gmail.com>
2018-11-29 01:46:00 +0100

    added email and passwords separately

--- applier.js
+++ applier.js
@@ -3,16 +3,19 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: false}); 
   const page = await browser.newPage()
+  const email="Jeher"
+  const password="password"
   
   //await page.screenshot({ path: 'keyboard.png' })
 
 
-  await page.goto('https://linkedin.com');
-  await page.focus('input.login-email');
-  await page.type('login'); // your login here
-  await page.focus('input.login-password');
-  await page.type('password'); // your password here
-  page.click('.submit-button');
+  await page.goto('https://linkedin.com', {"waitUntil" : "networkidle0"});
+  
+
+  await page.type('#login-email','login'); // your login here
+ 
+  await page.type('#login-password','password'); // your password here
+  page.click('#login-submit');
   await page.waitForNavigation();
   console.log('done!');
  
